35d78f7977af327fca3452c29086fae8ff993443
palmdrop <anton@exlex.se>
2021-01-28 19:22:05 +0100

    some experiments

--- src/sketch/texture/PatternStudySketch.java
+++ src/sketch/texture/PatternStudySketch.java
@@ -2,6 +2,7 @@ package sketch.texture;
 
 import color.colors.Colors;
 import color.space.drawer.MapColorSpaceDrawer;
+import processing.core.PApplet;
 import processing.core.PGraphics;
 import render.AbstractDrawer;
 import sampling.Sampler;
@@ -13,8 +14,10 @@ import sketch.Sketch;
 import util.geometry.Rectangle;
 import util.math.MathUtils;
 import util.noise.ComplexFractalHeightMap;
+import util.noise.FractalHeightMap;
 import util.noise.generator.GNoise;
 import util.noise.type.CellularNoise;
+import util.vector.Vector;
 
 import java.util.function.Supplier;
 
@@ -29,85 +32,74 @@ public class PatternStudySketch extends AbstractDrawer implements Sketch {
     }
 
     public PGraphics draw(PGraphics canvas, double frequency, boolean superSampling) {
-        Supplier<HeightMap> heightMapSupplier = () ->
-                HeightMaps.pow(
-                    HeightMaps.thresholdReverse(
-                        GNoise.simplexNoise(0.001, 1.0, 1.0), 0.3, false, true),
-                        HeightMaps.constant(0.15)
-                );
+        //double f = 0.01 + Math.random() * 0.1;
+        double f = 0.1;
+        double r =
+                //0;
+                Math.random() * PApplet.PI * 2;
 
+        Supplier<HeightMap> heightMapSupplier = () -> {
+            HeightMap h = HeightMaps.sin(
+                    Math.cos(r) * f,
+                    Math.sin(r) * f, 0.0, 1.0);
+            return HeightMaps.pow(h, HeightMaps.constant(0.5));
+        };
 
-        /*HeightMap warp = new GraphicsHeightMap(
-                canvas.parent.loadImage(
-                        ArrayAndListTools.randomElement(FileUtils.listFiles(
-                                "sourceImages/",
-                                new String[]{".png", ".jpg", ".jpeg"})).getPath()),
-                GraphicsSampler.WrapMode.MIRROR_WRAP,
-                Colors::brightness
-        );*/
+                /*HeightMaps.pow(
+                    HeightMaps.tan(
+                            Math.cos(r) * f,
+                            Math.sin(r) * f, 0.0, 1.0)
+                        .toModded().addSmoothMod(2.5)
+                        ,
+                    HeightMaps.constant(0.6)
+                );*/
 
         HeightMap controller =
                 heightMapSupplier.get();
-                //HeightMaps.stretch(heightMapSupplier.get(), 3, 3);
 
         HeightMap base1 = new ComplexFractalHeightMap(1.0,
                 1.0,
                 1.5,
                 HeightMaps.constant(1.0),
-                0.8,
+                0.5,
                 HeightMaps.stretch(controller, 2, 	2),
                 heightMapSupplier,
-                8).setNormalize(true);
+                1).setNormalize(true);
 
-
-        Supplier<HeightMap> shapeSupplier =
-                () -> {
-                    double f = MathUtils.random(0.0001, 0.02);
-                    return HeightMaps.stretch(
-                            CellularNoise.getInstance().toModded().addReverse(),
-                            f, f);
-                };
-
-        Sampler<Double> shape =
-                HeightMaps.add(shapeSupplier.get(), shapeSupplier.get(), shapeSupplier.get())
-                .toDistorted()
-                .domainWarp(
-                        base1,
-                        50
-                        );
-
-
-        //base1 =
-                //HeightMaps.add(shape, HeightMaps.mult(base1, 0.2));
-                //HeightMaps.mult(shape, base1, 0.5);
+        base1 = base1.toDistorted()
+        .domainWarp(
+                //base1,
+                //heightMapSupplier.get()
+                //GNoise.simplexNoise(0.01, 1.0, 1.0)
+                new FractalHeightMap(
+                        0.01,
+                        1.0,
+                        1.7,
+                        0.46,
+                        FractalHeightMap.Type.SIMPLEX,
+                        8,
+                        System.currentTimeMillis()
+                ),
+                GNoise.simplexNoise(0.01, 1.0, 1.0)
+                , 0, 10)
+                //.rotate(new Vector(500, 500), 2, 0.2)
+                //.sin(Math.random() * Math.PI * 2, 0.5, 0.5, 1)
+        ;
 
         Sampler<Double> texture =
                 base1;
 
-        double warpAmount = 50;
-        int recursionSteps = 2;
+        double warpAmount = 15;
+        int recursionSteps = 3;
 
-        double rotation = Math.random() * Math.PI * 2;
         for(int i = 0; i < recursionSteps; i++) {
             texture =
                     new WarpedHeightMap(base1).domainWarp(
                             texture,
-                            HeightMaps.constant(1),
+                            //HeightMaps.constant(1),
+                            texture,
                             0, warpAmount);
-
-            //texture = HeightMaps.pow(texture, HeightMaps.constant(0.7));
         }
-
-        texture = new NavelFabricPattern(
-                0.002,
-                0.1,
-                0.2,
-                1.5,
-                0.8,
-                50,
-                true
-        );
-
 
         HeightMap h =
                 GNoise.simplexNoise(0.001, 1.0, 1.0)
@@ -123,11 +115,11 @@ public class PatternStudySketch extends AbstractDrawer implements Sketch {
         double hEnd = hStart + (Math.random() > 0.5 ? -1 : 1) * MathUtils.random(0.3, 0.5);
 
         drawer.setComponentRange(0, hStart, hEnd);
-        drawer.setComponentRange(1, 0, 0.28);
+        drawer.setComponentRange(1, 0, 0.0);
         drawer.setComponentRange(2, 0.0, 1);
 
         drawer.setSuperSampling(superSampling);
-        drawer.draw(canvas, frequency * 1.0);
+        drawer.draw(canvas, frequency);
 
         return canvas;
     }
