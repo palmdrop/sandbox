24620684271bf6cdb67514081425585d8f181e83
palmdrop <anton@exlex.se>
2021-04-04 01:52:42 +0200

    Do some visualizations for blog

--- src/VisualizeMain.java
+++ src/VisualizeMain.java
@@ -1,14 +1,26 @@
+import color.colors.Colors;
+import organic.generation.points.PointGenerator;
+import organic.generation.points.area.HeightMapPointGenerator;
 import processing.core.PApplet;
 import processing.core.PGraphics;
+import processing.core.PImage;
 import render.Drawer;
 import render.heightMap.FadingHeightMapDrawer;
+import sampling.GraphicsHeightMap;
+import sampling.GraphicsSampler;
 import sampling.Sampler1D;
 import sampling.countour.Contours;
 import sampling.heightMap.HeightMap;
 import sampling.heightMap.HeightMaps;
+import sampling.heightMap.modified.FractalHeightMap;
+import text.TextTools;
 import util.geometry.Rectangle;
 import util.math.MathUtils;
 import util.noise.generator.GNoise;
+import util.noise.type.OpenSimplexNoise;
+import util.vector.Vector;
+
+import java.util.List;
 
 public class VisualizeMain extends PApplet {
     private int screenSize = 1000;
@@ -25,31 +37,25 @@ public class VisualizeMain extends PApplet {
     @Override
     public void setup() {
         canvas = createGraphics(screenWidth, screenHeight);
+        //PGraphics temp = createGraphics(screenWidth, screenHeight);
+        PImage temp = this.loadImage("sourceImages/collected/eye.jpg");
 
-        Rectangle bounds = new Rectangle(canvas.width, canvas.height);
+        //temp.beginDraw();
+        //temp.endDraw();
+
+        double scaleX = 1.0 * temp.width / canvas.width;
+        double scaleY = 1.0 * temp.height / canvas.height;
 
         HeightMap hm =
-                HeightMaps.pow(
-                    HeightMaps.thresholdReverse(GNoise.simplexNoise(0.01, 1.0, 1.0), 0.5, false, true),
-                        HeightMaps.constant(2.0)
+                HeightMaps.stretch(new GraphicsHeightMap(temp, GraphicsSampler.WrapMode.MIRROR_WRAP, Colors::brightness),
+                scaleX, scaleY);
+                //GNoise.simplexNoise(0.005, 1.0, 3.0);
+                /*HeightMaps.pow(
+                        new FractalHeightMap(0.005, 1.0, 2.0, 0.5, FractalHeightMap.Type.SIMPLEX, 8, System.currentTimeMillis()).setNormalize(true),
+                        HeightMaps.constant(4));*/
 
-                );
-                //new FractalHeightMap(0.005, 1.0, 2.0, 0.5, FractalHeightMap.Type.SIMPLEX, 8, System.currentTimeMillis())
-                /*new ComplexFractalHeightMap(1.0,
-                        1.0,
-                        2.0,
-                        HeightMaps.constant(1.0),
-                        0.7,
-                        GNoise.simplexNoise(0.005, 1.0, 0.5),
-                        () -> HeightMaps.thresholdReverse(GNoise.simplexNoise(0.003, 1.0, 1.0), 1.0, false, false),
-                        8)
-                .setNormalize(true);*/
-                //.toModded().addMod(4);
-
-        //hm = HeightMaps.mult(hm, GNoise.simplexNoise(0.01, 1.0, 1.0), 0.5);
-
-
-        Sampler1D<Double> contour = Contours.easing(MathUtils.EasingMode.EASE_IN, 0.3);
+        PointGenerator pg = new HeightMapPointGenerator(hm, screenWidth, screenHeight);
+        List<Vector> points = pg.generate(30000);
 
         Drawer drawer =
                 new FadingHeightMapDrawer(hm, 0, 0);
@@ -58,9 +64,17 @@ public class VisualizeMain extends PApplet {
 
 
         canvas.beginDraw();
-        canvas.background(255);
+        canvas.background(0);
 
-        drawer.draw(canvas, 1.0);
+        //drawer.draw(canvas, 1.0);
+
+        canvas.beginDraw();
+        canvas.strokeWeight(2);
+        canvas.stroke(255);
+        canvas.fill(255);
+        for(Vector p : points) {
+            canvas.point((float)p.getX(), (float)p.getY());
+        }
 
         canvas.endDraw();
     }
